fada56955e5a004ff6c3fc637bc4dabd21e6cf9f
lucas-sanmartino <lucas.sanmartino@globant.com>
2025-07-03 19:47:37 -0300

    Cambios Login 90% terminado

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE, // Ej: hack_academy_db
-  process.env.DB_USERNAME, // Ej: root
-  process.env.DB_PASSWORD, // Ej: root
+  process.env.DB_DATABASE || "api-ecomerce", // Ej: hack_academy_db
+  process.env.DB_USERNAME || "root", // Ej: root
+  process.env.DB_PASSWORD || "root", // Ej: root
   {
-    host: process.env.DB_HOST, // Ej: 127.0.0.1
-    dialect: process.env.DB_CONNECTION, // Ej: mysql
+    host: process.env.DB_HOST || "127.0.0.1", // Ej: 127.0.0.1
+    dialect: process.env.DB_CONNECTION || "mysql", // Ej: mysql
     logging: false, // Para que no aparezcan mensajes en consola.
   },
 );
@@ -15,7 +15,7 @@ const sequelize = new Sequelize(
 const User = require("./User");
 const Product = require("./Products");
 const Category = require("./Category");
-const Admin = require("./Admins")
+const Admin = require("./Admins");
 
 // Inicializar todos los modelos:
 User.initModel(sequelize);
@@ -25,14 +25,13 @@ Admin.initModel(sequelize);
 
 // Establecemos relaciones:
 
- Category.hasMany(Product);
- Product.belongsTo(Category);
-
+Category.hasMany(Product);
+Product.belongsTo(Category);
 
 module.exports = {
   sequelize,
   User,
   Product,
   Category,
-  Admin
+  Admin,
 };
